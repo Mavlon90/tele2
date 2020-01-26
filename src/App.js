8e23e707edc00cd2183708b25aa09d71fd785c04
Mavlon <Mavlon90@users.noreply.github.com>
2020-01-26 16:46:23 +0500

    Homwork 3 is added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Tarif from './tarifs/Tarif'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+   const tariffs = [
+       {id: 123, title: "Мой онлайн +", price: "700", chase: '₽', month: "месяц", 
+       title2: "безлимит на Tele2 России", purchase: "800 мин.", blockNum: "на остальные номера России", 
+       num: "30", gb: "ГБ интернета"},
+
+       {id: 123, title: "Везде онлайн", price: "500", chase: '₽', month: "месяц", 
+       title2: "безлимит на Tele2 России", purchase: "500 мин.", blockNum: "на остальные номера России", 
+       num: "40", gb: "ГБ интернета"},
+  
+       {id: 123, title: "Мой онлайн", price: "400", chase: '₽', month: "месяц", 
+       title2: "безлимит на Tele2 России", purchase: "500 мин.", blockNum: "на остальные номера России", 
+       num: "15", gb: "ГБ интернета"},
+  
+       {id: 123, title: "Мой разговор", price: "200", chase: '₽', month: "месяц", 
+       title2: "безлимит на Tele2 России", purchase: "200 мин.", blockNum: "на остальные номера России", 
+       num: "2", gb: "ГБ интернета"},
+  
+       {id: 123, title: "Мой Tele 2", price: "7", chase: '₽', month: "день", 
+       title2: "безлимит на Tele2 России", num: "5", gb: "ГБ интернета"},
+  
+   ];
+   
+    return (
+        <div>
+            <Tarif id={tariffs[0].id} title={tariffs[0].title} price={tariffs[0].price} chase={tariffs[0].chase} 
+            month={tariffs[0].month} title2={tariffs[0].title2} purchase={tariffs[0].purchase} blockNum={tariffs[0].blockNum}
+            num={tariffs[0].num} gb={tariffs[0].gb} />
+
+            <Tarif id={tariffs[1].id} title={tariffs[1].title} price={tariffs[1].price} chase={tariffs[1].chase} 
+            month={tariffs[1].month} title2={tariffs[1].title2} purchase={tariffs[1].purchase} blockNum={tariffs[1].blockNum}
+            num={tariffs[1].num} gb={tariffs[1].gb} />
+            
+
+            <Tarif id={tariffs[2].id} title={tariffs[2].title} price={tariffs[2].price} chase={tariffs[0].chase} 
+            month={tariffs[2].month} title2={tariffs[2].title2} purchase={tariffs[2].purchase} blockNum={tariffs[2].blockNum}
+            num={tariffs[2].num} gb={tariffs[2].gb} />
+
+            <Tarif id={tariffs[3].id} title={tariffs[3].title} price={tariffs[3].price} chase={tariffs[3].chase} 
+            month={tariffs[3].month} title2={tariffs[3].title2} purchase={tariffs[3].purchase} blockNum={tariffs[3].blockNum}
+            num={tariffs[3].num} gb={tariffs[3].gb} />
+
+            <Tarif id={tariffs[4].id} title={tariffs[4].title} price={tariffs[4].price} chase={tariffs[4].chase} 
+            month={tariffs[4].month} title2={tariffs[4].title2} purchase={tariffs[4].purchase} blockNum={tariffs[4].blockNum}
+            num={tariffs[4].num} gb={tariffs[4].gb} />
+        </div>
+    )
 }
-
-export default App;
---
+++ src/tarifs/Tarif.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function Tarif(props) {
+    return (
+    <article>
+        <div><h1>{props.title}</h1></div>
+                    <div><span>{props.price}</span>
+                        <div><span>{props.chase}</span>
+                                <span>{props.month}</span>
+                        </div>
+                    </div>
+                    <ul><li><div>
+                            <div>
+                                <div>{props.title2}</div>
+                            </div>
+                            <div>
+                                <li>
+                                <span>
+                                    <span>{props.purchase}</span>
+                                    <span>{props.blockNum}</span>
+                                </span>
+                                </li>
+                            </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <span>
+                                    <span>{props.num}</span>
+                                    <span>{props.gb}</span>
+                                </span>
+                            </div>
+                         </li>
+                    </ul>
+        </article>
+    )
+}
